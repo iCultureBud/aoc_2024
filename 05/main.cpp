76ca4d449a5e1bf958068e2985f80a6dae983d51
iCultureBud <99724871+iCultureBud@users.noreply.github.com>
2024-12-06 00:23:12 +0100

    day 5 part 2

--- 05/main.cpp
+++ 05/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
 	std::ifstream file(argv[1]);
 	std::string line;
 
-	if ( ! file.is_open() )
+	if ( !file.is_open() )
 		return -1;
 
 	uint32_t sum { 0 };
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 		if ( line.empty() )
 			break;
 
-		lookup[std::stoi(line.substr(0, 2))].push_back(std::stoi(line.substr(3, 2)));
+		lookup[ std::stoi(line.substr(0, 2)) ].push_back( std::stoi(line.substr(3, 2)) );
 	}
 
 	// Loop through codes and check conditions.
@@ -37,13 +37,13 @@ int main(int argc, char *argv[]) {
 		bool isValid { true };
 
 		while( getline(str, curr, ',') )
-			currCodes.push_back(std::stoi(curr));
+			currCodes.push_back( std::stoi(curr) );
 
 		for ( size_t idx = 0; idx < currCodes.size(); ++idx )
 		{
-			for ( uint32_t check : lookup[currCodes[idx]] )
+			for ( uint32_t check : lookup[ currCodes[idx] ] )
 			{
-				auto it = std::find(currCodes.begin(), currCodes.end(), check);
+				auto it = std::find( currCodes.begin(), currCodes.end(), check );
 
 				if ( it == currCodes.end() )
 					continue;
@@ -54,12 +54,20 @@ int main(int argc, char *argv[]) {
 				// If we get here the current check value is present BEFORE current code.
 				// So something must be wrong.
 				isValid = false;
-				break;
+
+				// Delete found false placed code.
+				currCodes.erase( it );
+
+				// We deleted something - so decrement idx.
+				--idx;
+
+				// Reinsert current check value behind current code.
+				currCodes.insert( currCodes.begin() + idx + 1, check );
 			}
 		}
 
-		if ( isValid )
-			sum += currCodes.at(currCodes.size() / 2);
+		if ( !isValid )
+			sum += currCodes.at( currCodes.size() / 2 );
 	}
 
 	std::cout << sum << '\n';
